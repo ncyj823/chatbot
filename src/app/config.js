f0cc1495dc80bd65ba2e8d43ee9538466ff48a3d
Kamal Walia <kamalwalia.dev@gmail.com>
2025-01-17 07:56:20 +0530

    chatbot

---
+++ src/app/config.js
@@ -0,0 +1,13 @@
+import { createChatBotMessage } from "react-chatbot-kit";
+
+const config = {
+  botName: "Credit Card Advisor",
+  initialMessages: [
+    createChatBotMessage(`Hello I can help you in deciding what Credit Card fits best for you`)
+  ]
+}
+
+export default config
+
+
+// bot Avatar
